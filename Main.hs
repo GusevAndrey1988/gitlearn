c99bdae98b07ccd566cd80d84d6bb95c6ffa66e5
Гусев Андрей <gusevandrey1988@gmail.com>
2025-02-26 01:16:24 +0300

    upd 1

--- Main.hs
+++ Main.hs
@@ -2,6 +2,18 @@
 
 module Main (main) where
 
+import GHC.Conc (par)
+import System.Environment (getArgs)
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = f1 `par` f2 `seq` f1 + f2 
+  where
+    f1 = fib (n - 1)
+    f2 = fib (n - 2)
+
 main :: IO ()
 main = do
-  putStrLn "Hello, World!"
+  (n : _) <- getArgs
+  putStrLn $ "fib(" <> n <> ")=" <> show (fib $ read n)
