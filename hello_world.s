eee3f991e5c8673d00c4c069723d4d7021135311
Гусев Андрей <gusevandrey1988@gmail.com>
2025-03-05 06:19:38 +0300

    add comment

--- hello_world.s
+++ hello_world.s
@@ -6,11 +6,13 @@ len: .quad 15
 
 .section .text
 _start:
+    // print message
     movq $message, %rsi
     movq len, %rdx
     movq $1, %rdi
     movq $0x1, %rax
     syscall
 
+    // exit
     movq $0x3c, %rax
     syscall
